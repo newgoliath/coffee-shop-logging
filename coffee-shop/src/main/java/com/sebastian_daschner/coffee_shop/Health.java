8a38b6881366575f0b69484fbb37d4e237230dd8
Judd Maltin <judd@newgoliath.com>
2021-07-12 15:53:34 -0400

    simple json logging

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -6,12 +6,17 @@ import org.eclipse.microprofile.health.Readiness;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import java.util.logging.Level;
+
 @Readiness
 @ApplicationScoped
 public class Health implements HealthCheck {
 
+    java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
+
     @Override
     public HealthCheckResponse call() {
+        logger.info("Calling HealthCheckResponse");
         return HealthCheckResponse.up("coffee-shop");
     }
 
