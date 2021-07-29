f90e45899447877a02b95ea75aeeac600e2b87fa
Cloud User <ec2-user@bastion.judd32.internal>
2021-07-29 01:09:21 +0000

    order status in json

--- barista/src/main/java/com/sebastian_daschner/barista/OrderStatusProcessor.java
+++ barista/src/main/java/com/sebastian_daschner/barista/OrderStatusProcessor.java
@@ -2,20 +2,30 @@ package com.sebastian_daschner.barista;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import java.util.logging.Level;
+
 @ApplicationScoped
 public class OrderStatusProcessor {
+
+    java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
 
     public String process(final String status) {
         switch (status) {
             case "PREPARING":
+                logger.info("FINISHED order");
                 return "FINISHED";
             case "FINISHED":
+                logger.info("COLLECTED order");
                 return "COLLECTED";
             case "COLLECTED":
+                logger.info("COLLECTED order");
                 return "COLLECTED";
             default:
+                logger.info("Unknown status order" + status);
                 throw new IllegalArgumentException("Unknown status " + status);
         }
     }
 
 }
+
+
