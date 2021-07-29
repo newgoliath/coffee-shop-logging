5dd7185358cf7f1e3668351e141c876a1d856758
Cloud User <ec2-user@bastion.judd32.internal>
2021-07-29 00:56:13 +0000

    try barista json logging

--- barista/src/main/java/com/sebastian_daschner/barista/HealthCheckResource.java
+++ barista/src/main/java/com/sebastian_daschner/barista/HealthCheckResource.java
@@ -3,12 +3,18 @@ package com.sebastian_daschner.barista;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import java.util.logging.Level;
+
 @Path("health")
 public class HealthCheckResource {
 
+    java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
+
     @GET
     public String healthCheck() {
+        logger.info("HealthCheck from Barista");
         return "OK";
     }
 
 }
+
